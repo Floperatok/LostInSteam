a6fb584c1bf9d5cea850ab9e0747bf79b4a9536c
Floperatox <floperatox@gmail.com>
2025-02-08 23:07:40 +0100

    add switch pano dev button

--- mysite/geogamers/static/js/main.js
+++ mysite/geogamers/static/js/main.js
@@ -1,14 +1,28 @@
 
 document.addEventListener("DOMContentLoaded", function() {
-    var panoElement = document.getElementById('pano');
-    var viewer = new Marzipano.Viewer(panoElement);
-    var geometry = new Marzipano.EquirectGeometry([{ width: 8192, height: 4096 }]);
-    var source = Marzipano.ImageUrlSource.fromString("/static/pano/middle_earth/0/pano.jpg");
-    var scene = viewer.createScene({
+
+
+
+    let panoElement = document.getElementById("pano");
+    let viewer = new Marzipano.Viewer(panoElement);
+    let geometry = new Marzipano.EquirectGeometry([{ width: 8192, height: 4096 }]);
+    let source = Marzipano.ImageUrlSource.fromString("/static/pano/middle_earth/0/pano.jpg");
+    let scene = viewer.createScene({
         source: source,
         geometry: geometry,
-        view: new Marzipano.RectilinearView() // Type de vue rectilinéaire
+        view: new Marzipano.RectilinearView()
     });
-
     scene.switchTo();
+
+    let devChangePanoButton = document.getElementById("dev_change_pano_button");
+    devChangePanoButton.addEventListener("click", function(e) {
+        geometry = new Marzipano.EquirectGeometry([{ width: 7168, height: 3584 }]);
+        source = Marzipano.ImageUrlSource.fromString("/static/pano/sea_of_thieves/0/pano.jpg");
+        scene = viewer.createScene({
+            source: source,
+            geometry: geometry,
+            view: new Marzipano.RectilinearView()
+        });
+        scene.switchTo();
+    });
 })
