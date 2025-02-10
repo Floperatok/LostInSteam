c493543c8f24cf47780a39b600285def0737b158
NOure <noor-wawa@hotmail.com>
2025-02-10 18:24:32 +0100

    added logo

--- mysite/geogamers/urls.py
+++ mysite/geogamers/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("game/", views.game, name="game"),
 ]
--- mysite/geogamers/views.py
+++ mysite/geogamers/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "index.html")
+
+def game(request):
+    return render(request, "game.html")
