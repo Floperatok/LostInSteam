a32e4bfeefd0e11b41662bbe3e5838215663a99e
Floperatox <floperatox@gmail.com>
2025-02-15 16:44:24 +0100

    new map model ; update_panorams script code refactor

--- mysite/geogamers/management/commands/update_panoramas.py
+++ mysite/geogamers/management/commands/update_panoramas.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
-from geogamers.models import Game, Pano
+from geogamers.models import Game, Pano, Map
 import os, json
 
 class Command(BaseCommand):
@@ -7,6 +7,75 @@ class Command(BaseCommand):
 
 	def add_arguments(self, parser):
 		parser.add_argument("data_path", type=str)
+
+
+	def create_pano_obj(self, pano_data, game_obj):
+		pano_settings_path = f"{pano_data["path"]}/data.json"
+
+		if not os.path.exists(pano_settings_path):
+			self.stderr.write(self.style.ERROR(f"Error: {pano_settings_path} not found"))
+			return
+
+		pano_settings_file = open(pano_settings_path)
+		pano_settings = json.load(pano_settings_file)
+
+		try:
+			pano_obj, created = Pano.objects.get_or_create(
+				game = game_obj,
+				number = pano_data["number"],
+				posx = pano_data["posx"],
+				posy = pano_data["posy"],
+				settings = pano_settings,
+			)
+		except Exception as err:
+			self.stderr.write(self.style.ERROR(f"Error creating panoramas {pano_data["number"]} for the game {game_obj.name}: {err}"))
+			return
+
+		if created:
+			self.stdout.write(self.style.SUCCESS(f"New panorama           : {pano_obj.__str__()}"))
+		else:
+			self.stdout.write(f"Panorama already exist : {pano_obj.__str__()}")
+
+	
+	def create_map_obj(self, map_data, game_obj, game_path):
+		map_path = f"{game_path}/map"
+		try:
+			map_obj, created = Map.objects.get_or_create(
+				game = game_obj,
+				max_zoom = map_data["max_zoom"],
+			)
+		except Exception as err:
+			self.stderr.write(self.style.ERROR(f"Error creating map for the game {game_obj.name}: {err}"))
+			return
+
+		if created:
+			self.stdout.write(self.style.SUCCESS(f"New map                : {map_obj.__str__()}"))
+		else:
+			self.stdout.write(f"Map already exist      : {map_obj.__str__()}")
+
+
+	def create_game_obj(self, game_data):
+		try:
+			game_obj, created = Game.objects.get_or_create(
+				name = game_data["name"],
+				pretty_name = game_data["pretty_name"],
+				accepted_names = game_data["accepted_names"],
+			)
+		except Exception as err:
+			self.stderr.write(self.style.ERROR(f"Error creating game {game_data["name"]}: {err}"))
+			return
+
+		if created:
+			self.stdout.write(self.style.SUCCESS(f"New game               : {game_obj.__str__()}"))
+		else:
+			self.stdout.write(f"Game already exist     : {game_obj.__str__()}")
+		
+		self.create_map_obj(game_data["map"], game_obj, game_data["path"])
+
+		for pano_data in game_data["panoramas"]:
+			pano_data["path"] = f"{game_data["path"]}/{pano_data["number"]}"
+			self.create_pano_obj(pano_data, game_obj)
+
 
 	def handle(self, *args, **options):
 		data_path = f"{options["data_path"].strip("/ ")}/data.json"
@@ -17,40 +86,6 @@ class Command(BaseCommand):
 		
 		data_file = open(data_path, "r")
 		data = json.load(data_file)
-		for game in data:
-			game_obj, created = Game.objects.get_or_create(
-				name = game["name"],
-				pretty_name = game["pretty_name"],
-				accepted_names = game["accepted_names"],
-			)
-			if created:
-				self.stdout.write(self.style.SUCCESS(f"New game               : {game_obj.__str__()}"))
-			else:
-				self.stdout.write(f"Game already exist     : {game_obj.__str__()}")
-			
-			for pano in game["panoramas"]:
-				pano_settings_path = (
-					f"{options["data_path"].strip("/ ")}"
-					f"/{game["name"].strip("/ ")}"
-					f"/{pano["number"]}"
-					f"/data.json"
-				)
-
-				if not os.path.exists(pano_settings_path):
-					self.stdout.write(self.style.ERROR(f"Error: {pano_settings_path} not found"))
-					continue
-
-				pano_settings_file = open(pano_settings_path)
-				pano_settings = json.load(pano_settings_file)
-
-				pano_obj, created = Pano.objects.get_or_create(
-					game = game_obj,
-					number = pano["number"],
-					posx = pano["posx"],
-					posy = pano["posy"],
-					settings = pano_settings,
-				)
-				if created:
-					self.stdout.write(self.style.SUCCESS(f"New panorama           : {pano_obj.__str__()}"))
-				else:
-					self.stdout.write(f"Panorama already exist : {pano_obj.__str__()}")
+		for game_data in data:
+			game_data["path"] = options["data_path"].rstrip("/ ") + "/" + game_data["name"]
+			self.create_game_obj(game_data)
--- mysite/geogamers/models.py
+++ mysite/geogamers/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 import uuid
 
-# Create your models here.
-
 class Game(models.Model):
 	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 	name = models.CharField(max_length=25, default="")
@@ -10,19 +8,28 @@ class Game(models.Model):
 	accepted_names = models.JSONField(default=list)
 	
 	def __str__(self):
-		return f'GameObj {self.id} ; "{self.name}"'
+		return f"GameObj {self.id} ; '{self.name}'"
+
 
 class Pano(models.Model):
 	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 	number = models.PositiveIntegerField(default=0)
-	game = models.ForeignKey(Game, on_delete=models.CASCADE)
 	posx = models.FloatField(default=0)
 	posy = models.FloatField(default=0)
 	settings = models.JSONField(default=list)
+	game = models.ForeignKey(Game, on_delete=models.CASCADE)
 
 	class Meta:
 		unique_together = ('game', 'number')
 
 	def __str__(self):
-		return f'PanoObj {self.id} ; "{self.game.name}" number={self.number} x={self.posx} y={self.posy}'
+		return f"PanoObj {self.id} ; '{self.game.name}' number={self.number} x={self.posx} y={self.posy}"
 	
+
+class Map(models.Model):
+	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+	max_zoom = models.PositiveIntegerField(default=7)
+	game = models.ForeignKey(Game, on_delete=models.CASCADE)
+	
+	def __str__(self):
+		return f"MapObj {self.id} ; '{self.game.name}'"
