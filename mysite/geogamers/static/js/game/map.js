42b6f5900c543d383b831a58734842650878daaa
Floperatox <floperatox@gmail.com>
2025-03-07 18:26:27 +0100

    disable marker placement and change cursor back to default in result screen

--- mysite/geogamers/static/js/game/map.js
+++ mysite/geogamers/static/js/game/map.js
@@ -24,25 +24,33 @@ function logMapLayers(map) {
 }
 
 
-function addMarkerOnClick(map) {
-	var guessPosBtn = document.getElementById('guess_pos_btn')
-
-	function onMapClick(e) {
-		guessPosBtn.style.display = "block";
-		if (mapLayerGroup.getLayers().length > 0) {
-			mapLayerGroup.clearLayers();
-		}
-		// guessMarker = new L.marker(e.latlng).addTo(map);
-		L.marker(e.latlng).addTo(mapLayerGroup).bindPopup(`${e.latlng}`);
+function onMapClick(e) {
+	document.getElementById('guess_pos_btn').style.display = "block";
+	if (mapLayerGroup.getLayers().length > 0) {
+		mapLayerGroup.clearLayers();
 	}
+	L.marker(e.latlng).addTo(mapLayerGroup).bindPopup(`${e.latlng}`);
+}
 
-	guessPosBtn.addEventListener('click', function() {
+
+function disableMarkerOnClick(map) {
+	map.off('click', onMapClick);
+}
+
+
+function enableMarkerOnClick(map) {
+	const guessPosBtn = document.getElementById('guess_pos_btn');
+
+	guessPosBtn.removeEventListener('click', handleDisableMarkerOnButtonPress);
+	guessPosBtn.addEventListener('click', handleDisableMarkerOnButtonPress);
+	map.on('click', onMapClick);
+
+	function handleDisableMarkerOnButtonPress(event) {
 		map.off('click', onMapClick);
 		setTimeout(() => {
 			map.on('click', onMapClick);
 		}, 10);
-	});
-	map.on('click', onMapClick);
+	}
 }
 
 
@@ -60,7 +68,6 @@ async function loadMap(map, mapId, container) {
 			map.removeLayer(layer);
 		}
 	});
-
 
 	var baseUrl = `/api/map/${mapData.id}`;
 	L.tileLayer(`${baseUrl}/{z}/{x}/{y}.jpg`, {
@@ -97,6 +104,8 @@ async function displayResultMap(map, container, result) {
 		container.classList.toggle("map_ingame");
 	}
 
+	disableMarkerOnClick(map);
+
 	const guessMarker = mapLayerGroup.getLayers()[0];
 	const icon = L.icon({
 		iconUrl: '/static/image/marker-icon.png',
@@ -110,6 +119,7 @@ async function displayResultMap(map, container, result) {
 		color: "white",
 		dashArray: "20, 20",
 	}).addTo(mapLayerGroup);
+	L.DomUtil.removeClass(map._container, 'crosshair-cursor-enabled');
 
 	map.setView(polyline.getCenter(), map.getBoundsZoom(polyline.getBounds()));
 	setTimeout(function(){ map.invalidateSize(true)}, 100);
@@ -127,8 +137,8 @@ async function displayMinimap(map, container) {
 
 	container.style.display = "block";
 	
-	addMarkerOnClick(map);
-	L.DomUtil.addClass(map._container,'crosshair-cursor-enabled');
+	enableMarkerOnClick(map);
+	L.DomUtil.addClass(map._container, 'crosshair-cursor-enabled');
 	map.setView([0, 0], 3);
 	map.invalidateSize()
 }
