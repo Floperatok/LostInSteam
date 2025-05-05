fa31f219c590865799d5aacdd67375ba836881bc
Floperatox <floperatox@gmail.com>
2025-05-05 13:55:53 +0200

    remove django secret key

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s)!+)@(p9ojpf7b-qpi4-1co$o^l1spo6dymzl#8c_+4@sor6$'
+SECRET_KEY = ''
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
